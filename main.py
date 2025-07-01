3c05dc8197f5966645957caa7ef993018d8f3d7c
Threat <dothanhtin22@gmail.com>
2025-07-01 23:27:58 +0700

    update

--- main.py
+++ main.py
@@ -121,8 +121,8 @@ class SEOContentPipeline:
         wp_password = siteResult["password"]
 
         #Ghi log chỗ này
-        # logger.info(f"wp_domain_url: {wp_domain_url}, wp_url: {wp_url}, full_url: {url}")
-        # logger.info(f"wp_username: {wp_username}, wp_password: {wp_password}")
+        logger.info(f"wp_domain_url: {wp_domain_url}, wp_url: {wp_url}, full_url: {url}")
+        logger.info(f"wp_username: {wp_username}, wp_password: {wp_password}")
 
         wp_client = Client(url, wp_username, wp_password)
         post_id = wp_client.call(NewPost(post))
